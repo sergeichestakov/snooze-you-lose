fcf8ba7587620644c0345ea54088963fca358b76
sergeichestakov <me@sergei.com>
2024-03-30 21:00:54 -0400

    Add custom fonts

--- app/index.tsx
+++ app/index.tsx
@@ -4,17 +4,19 @@ import { StyleSheet, Text, View, Pressable } from "react-native";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>It's simple</Text>
-      <Text>You snooze</Text>
-      <Text>you lose</Text>
-      <Text>Real money</Text>
+    <View style={styles.root}>
+      <Text style={styles.captionSmall}>It's simple</Text>
+      <Text style={styles.captionLarge}>You snooze</Text>
+      <Text style={styles.captionLarge}>You lose</Text>
+      <Text style={styles.captionSmall}>Real money</Text>
 
-      <Link href="/selectTime" asChild>
-        <Pressable>
-          <Text>Begin Challenge</Text>
-        </Pressable>
-      </Link>
+      <View>
+        <Link href="/selectTime" asChild>
+          <Pressable>
+            <Text>Begin Challenge</Text>
+          </Pressable>
+        </Link>
+      </View>
 
       <StatusBar style="auto" />
     </View>
@@ -22,10 +24,21 @@ export default function Home() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  captionSmall: {
+    fontSize: 32,
+    textTransform: "uppercase",
+    fontFamily: "HighriseFont-Demo-Regular",
+  },
+
+  captionLarge: {
+    fontSize: 56,
+    textTransform: "uppercase",
+    fontFamily: "HighriseFont-Demo-Regular",
   },
 });
